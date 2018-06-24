b1b94fc6fc4f4bac876a5408e29bfa25f74ebfc5
Harisha12 <31131848+Harisha12@users.noreply.github.com>
2018-06-24 18:19:14 +0530

    Update StartActivity.java

--- Desktop/VirtusaQuiz/app/src/main/java/com/example/harisha/virtusaquiz/StartActivity.java
+++ Desktop/VirtusaQuiz/app/src/main/java/com/example/harisha/virtusaquiz/StartActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Calendar;
+
 public class StartActivity extends AppCompatActivity {
 
     public Button button;
+    Calendar calendar = Calendar.getInstance();
+    int day = calendar.get(Calendar.DAY_OF_WEEK);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,12 @@ public class StartActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(StartActivity.this, MainActivity.class));
+                if(day == Calendar.SATURDAY || day==Calendar.SUNDAY) {
+                    startActivity(new Intent(StartActivity.this,NoTestActivity.class));
+
+                } else {
+                    startActivity(new Intent(StartActivity.this, MainActivity.class));
+                }
             }
         });
 
